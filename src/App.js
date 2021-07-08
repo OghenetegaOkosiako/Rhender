b8938aea888f543475525eee6b9b2c42ec64f1ad
Precious <prechieb@gmail.com>
2021-07-08 14:17:09 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col}  from "react-bootstrap";
 import './App.css';
+import TopNav from "./components/Navbar.js"
+import TopCarousel from "./components/TopCarousel"
+import Contacts from "./components/Contacts";
+import Services from "./components/Services";
+import AppDevelopment from "./components/AppDevelopment"
+import SocialMedia from "./components/SocialMedia"
+import Seo from "./components/Seo"
+import Footer from "./components/Footer"
+import PrivacyPolicy from './components/PrivacyPolicy';
+import TermsOfService from "./components/TermsOfService"
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-background">
+      <Router>
+      <Switch>
+      < Route path="/" exact component ={TopCarousel}/>
+      <Route path="/termsofservice" component={TermsOfService} />
+      <Route path="/privacypolicy" component={PrivacyPolicy} />
+      </Switch>
+      </Router>
     </div>
+    
   );
 }
 
